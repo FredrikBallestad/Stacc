487c2c261ecb72c193a9f09bfab52245c7798ede
Fredrik Ballestad <fba037@uib.no>
2023-10-20 03:11:11 +0200

    made the future value return the right answer

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask, json
 from flask_cors import CORS
+import math as Math
 
 from flask import (
     Flask,
@@ -26,11 +27,18 @@ def backend_request():
         monthly_investment = data["monthly_investment"]
         saving_duration = data["saving_duration"]
         annual_return_percentage = data["annual_return_percentage"]
+        annual_return_percentage = annual_return_percentage / 100
 
         if None in (starting_investment, monthly_investment, saving_duration, annual_return_percentage):
             return jsonify({"error": "Missing or invalid data"}), 400  # Returner en feilmelding
+        
+        future_value_starting_investment = starting_investment * (1 + annual_return_percentage) ** saving_duration
 
-        future_value = (starting_investment * (1 + annual_return_percentage / 100) ** saving_duration) + (monthly_investment * saving_duration * 12 * annual_return_percentage / 12)
+        future_value_on_monthly_investments = monthly_investment * (1 + annual_return_percentage/12)
+        for i in range(0, (saving_duration*12-1)):
+            future_value_on_monthly_investments = (future_value_on_monthly_investments + monthly_investment) * (1 + annual_return_percentage/12)
+
+        future_value = future_value_starting_investment + future_value_on_monthly_investments
         return jsonify({"future value" : future_value})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
